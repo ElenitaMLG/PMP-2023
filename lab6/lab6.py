658087a1e79c48e17830a3ab575cc655be31e283
ElenitaMLG <100302422+ElenitaMLG@users.noreply.github.com>
2023-11-07 12:34:12 +0200

    Create lab6.py

---
+++ lab6/lab6.py
@@ -0,0 +1,29 @@
+import pymc as pm
+import arviz as az
+import matplotlib.pyplot as plt
+
+
+def model_poisson_binomial(y, theta):
+    with pm.Model() as model:
+        n = pm.Poisson('n', mu=10)
+        y_obs = pm.Binomial('y_obs', n=n, p=theta, observed=y)
+        trace = pm.sample(1000, return_inferencedata=True)
+
+    return trace
+
+
+results = {}
+combinations = [(y, theta) for y in [0, 5, 10] for theta in [0.2, 0.5]]
+
+for y, theta in combinations:
+    trace = model_poisson_binomial(y, theta)
+    results[(y, theta)] = trace
+
+fig, axes = plt.subplots(len(combinations) // 2, 2, figsize=(10, 8), squeeze=False)
+for i, ((y, theta), trace) in enumerate(results.items()):
+    ax = axes[i // 2, i % 2]
+    az.plot_posterior(trace, var_names=['n'], ax=ax)
+    ax.set_title(f'Y={y}, θ={theta}')
+
+plt.tight_layout()
+plt.show()
